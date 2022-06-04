1bc6516408a49ff633f441a36dfad2ae38ff73bd
TrishLeaf <trishanp96@gmail.com>
2022-06-05 01:26:21 +0530

    added routes

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('build the server')
-    res.end('Home')
+    const url = req.url
+    if(url === '/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>home</h1>')
+        res.end()
+    }
+    else if(url === '/about'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>about</h1>')
+        res.end()
+    }
+    else{
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write('<h1>page not found</h1>')
+        res.end()
+
+    }
+    
 })
 
 server.listen(5000)
